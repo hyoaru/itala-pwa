7fd6d728ee2a90851c273bc304efd9e2fd734530
hyoaru <the.jadecabrera@gmail.com>
2026-08-27 20:27:16 +0800

    chore: add tabbed txs

--- src/infrastructure/router/routes/dashboard.tsx
+++ src/infrastructure/router/routes/dashboard.tsx
@@ -1,6 +1,7 @@
 import { useAuthenticationSessionContext } from "@/infrastructure/contexts/authentication-session";
-import { Button } from "@heroui/react";
+import { Button, Tabs } from "@heroui/react";
 import { createFileRoute, Link, redirect } from "@tanstack/react-router";
+import { ArrowUpRight, SquareArrowUpRight } from "lucide-react";
 
 export const Route = createFileRoute("/dashboard")({
   beforeLoad: ({ context }) => {
@@ -27,6 +28,11 @@ function RouteComponent() {
   const { user } = useAuthenticationSessionContext();
   const greeting = getGreeting();
   const initials = (user?.firstName ?? "A")?.[0] + user?.lastName?.[0];
+  const transactionTabs = [
+    { name: "all" },
+    { name: "income" },
+    { name: "expense" },
+  ];
 
   return (
     <>
@@ -39,7 +45,7 @@ function RouteComponent() {
             <p className="p-1 text-xs font-semibold">{initials}</p>
           </div>
         </div>
-        <div className="bg-default-soft rounded-3xl p-5">
+        <div className="bg-default rounded-3xl p-5">
           <div className="space-y-1">
             <p className="text-muted text-sm">Available balance</p>
             <p className="font-heading text-4xl font-semibold">₱4,280.50</p>
@@ -59,6 +65,34 @@ function RouteComponent() {
           <p className="font-heading text-sm font-medium">
             Recent transactions
           </p>
+          <Tabs className="">
+            <div className="flex items-center">
+              <div className="grow">
+                <Tabs.ListContainer className="bg-default w-max">
+                  <Tabs.List className="**:data-[slot=tabs-indicator]:bg-accent">
+                    {transactionTabs.map((item) => (
+                      <Tabs.Tab
+                        className="capitalize"
+                        key={item.name}
+                        id={item.name}
+                      >
+                        {item.name}
+                        <Tabs.Indicator />
+                      </Tabs.Tab>
+                    ))}
+                  </Tabs.List>
+                </Tabs.ListContainer>
+              </div>
+              <Button isIconOnly size="sm" variant="secondary" className="">
+                <ArrowUpRight className="size-6" />
+              </Button>
+            </div>
+            {/* {items.map((item) => ( */}
+            {/*   <Tabs.Panel key={item.id} className="pt-4" id={item.id}> */}
+            {/*     <p>{item.label} panel content.</p> */}
+            {/*   </Tabs.Panel> */}
+            {/* ))} */}
+          </Tabs>
           <div className="space-y-2">
             <div className="bg-default-soft flex justify-between rounded-3xl p-3 text-sm">
               <p className="font-medium">Coffee shop</p>
