5eafd5d34b5930114e57cab210c91311229a1e70
hyoaru <the.jadecabrera@gmail.com>
2026-08-31 11:32:19 +0800

    chore: ui improvements tx icon

--- src/infrastructure/components/transactions-panel.tsx
+++ src/infrastructure/components/transactions-panel.tsx
@@ -1,7 +1,8 @@
-import { ScrollShadow, Tabs } from "@heroui/react";
+import { cn, ScrollShadow, Tabs } from "@heroui/react";
 import { useSuspenseInfiniteQuery } from "@tanstack/react-query";
 import { useTransactionActions } from "../hooks";
 import { TransactionType } from "@/domain/value-objects";
+import { BanknoteArrowDown, BanknoteArrowUp } from "lucide-react";
 
 type Query = Parameters<
   ReturnType<typeof useTransactionActions>["findTransactionsInfinite"]
@@ -22,17 +23,33 @@ export const TransactionsPanel = (props: TransactionsPanelProps) => {
     <>
       <Tabs.Panel className="h-full p-0" id={props.id}>
         <ScrollShadow className="h-full space-y-2">
-          {transactions.map((t) => {
+          {transactions.map((t, i) => {
             return (
               <div
-                key={`AllTransactions-${t.id}`}
-                className="bg-default-soft flex justify-between rounded-3xl p-3 text-sm"
+                key={`Transaction-${t.id}`}
+                className="flex items-center gap-2 px-3"
               >
-                <p className="font-medium">{t.description}</p>
-                <p className="font-semibold">
-                  {t.type == TransactionType.Income ? "+" : "-"}₱
-                  {Number(t.amount).toLocaleString()}
-                </p>
+                {t.type == TransactionType.Income ? (
+                  <div className="relative">
+                    <div className="bg-success/50 absolute inset-0 m-auto size-1.5 rounded-full"></div>
+                    <BanknoteArrowUp className="text-muted/50 h-[1.6em] w-[1.6em]" />
+                  </div>
+                ) : (
+                  <div className="relative">
+                    <div className="bg-danger/50 absolute inset-0 m-auto size-1.5 rounded-full"></div>
+                    <BanknoteArrowDown className="text-muted/50 h-[1.6em] w-[1.6em]" />
+                  </div>
+                )}
+                <div className="bg-default-soft flex flex-1 justify-between rounded-3xl p-3 text-sm">
+                  <div className="flex items-center gap-1">
+                    <p className="font-medium">{t.description}</p>
+                  </div>
+                  <p className="font-semibold">
+                    {t.type == TransactionType.Income ? "+" : "-"}
+                    {" ₱"}
+                    {Number(t.amount).toLocaleString()}
+                  </p>
+                </div>
               </div>
             );
           })}
