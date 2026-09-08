b676b6708bb8dd070eb53504dc7fcd1904c077e8
hyoaru <the.jadecabrera@gmail.com>
2026-09-08 17:29:21 +0800

    fix: balance toggle a little too hard to click

--- src/infrastructure/components/account-balance-card.tsx
+++ src/infrastructure/components/account-balance-card.tsx
@@ -27,7 +27,7 @@ export const AccountBalanceCard = (props: AccountBalanceCardProps) => {
             </p>
           )}
           <Button
-            className="h-max w-max ps-2"
+            className="h-max justify-end"
             isIconOnly
             size="sm"
             variant="ghost"
